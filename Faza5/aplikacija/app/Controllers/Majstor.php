cdc140d778b84747b6c60b0534da4570fb38f4e8
samoluka <sl180053d@student.etf.bg.ac.rs>
2021-05-20 20:01:28 +0200

    dodao stranicu sa kalendarom

--- Faza5/aplikacija/app/Controllers/Majstor.php
+++ Faza5/aplikacija/app/Controllers/Majstor.php
@@ -13,55 +13,75 @@ namespace App\Controllers;
  *
  * @author Windows User
  */
+
 use App\Models\UslugaModel;
 
-class Majstor extends BaseController {
-    
-    protected function prikaz($stranica, $podaci){
-        $podaci['controller'] ="Majstor";
+class Majstor extends BaseController
+{
+
+    protected function prikaz($stranica, $podaci)
+    {
+        $podaci['controller'] = "Majstor";
         $podaci['ime'] = 'Code';
-        $podaci['prezime']='Igniter';
+        $podaci['prezime'] = 'Igniter';
         echo view("osnova/header");
         echo view("majstor/meni", $podaci);
         echo view("majstor/$stranica", $podaci);
         echo view("osnova/footer");
     }
-    
-    
-    public function dodajUslugu(){
-        $this->prikaz("dodavanjeusluga",[]);
-        
-        
-       // echo($this->request->getVar("naslov"));
-       
+
+
+    public function dodajUslugu()
+    {
+        $this->prikaz("dodavanjeusluga", []);
+
+
+        // echo($this->request->getVar("naslov"));
+
         //redirect()->to(site_url("Majstor/novaUsluga"));
     }
-    
-    public function novaUsluga(){
-        
+
+    public function novaUsluga()
+    {
+
         //napraviti uslugaModel i to ubaciti u bazu posle provere ispravnosti podataka
-        
+
         $uslugaModel = new UslugaModel();
         $uslugaModel->save([
-            'idUsl'=>1,
-            'naziv'=>$this->request->getVar('naslov'),
-            'opis'=>$this->request->getVar('opis'),
-            'cena'=>$this->request->getVar('cena'),
-            'idMaj'=>1
+            'idUsl' => 1,
+            'naziv' => $this->request->getVar('naslov'),
+            'opis' => $this->request->getVar('opis'),
+            'cena' => $this->request->getVar('cena'),
+            'idMaj' => 1
         ]);
-        
-        
-        echo ("dodata je nova usluga:");
+
+
+        echo("dodata je nova usluga:");
         echo($this->request->getVar("naslov"));
         echo($this->request->getVar('opis'));
         echo($this->request->getVar("cena"));
-        
-        
+
+
     }
-    
-    public function mojeUsluge(){
-        
+
+    public function mojeUsluge()
+    {
+
         $uslugaModel = new UslugaModel();
-        echo ($uslugaModel->getInsertId());
+        echo($uslugaModel->getInsertId());
     }
+
+    public function kalendar()
+    {
+        $termini = [];
+        for ($i = 0; $i < 4; $i++) {
+            for ($j = 0; $j < 3; $j++) {
+                $termin = new \App\Libraries\KalendarTermin((($i * 3 + $j) * 2) . "-" . (($i * 3 + $j) * 2 + 2));
+                array_push($termini, $termin);
+            }
+        }
+        $data["termini"] = $termini;
+        $this->prikaz("kalendar", $data);
+    }
+
 }
