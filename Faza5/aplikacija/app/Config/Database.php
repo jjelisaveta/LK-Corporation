5637bd92415b6cd8163b3c5733807e2e22015721
unknown <stefanpajovic1999@gmail.com>
2021-05-22 14:30:26 +0200

    presao na svoj branch
    branch za registrovanje i komentare

--- Faza5/aplikacija/app/Config/Database.php
+++ Faza5/aplikacija/app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
 		'DSN'      => '',
 		'hostname' => 'localhost',
 		'username' => 'root',
-		'password' => '',
+		'password' => 'Stefan2999',
 		'database' => 'popravi',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
