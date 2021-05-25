c0ac44df283b04f44dd3d640259ca50560679fe7
FansteLK <stefanpajovic1999@gmail.com>
2021-05-25 09:59:01 +0200

    prebacene skripte u js fajl

--- Faza5/aplikacija/app/Views/klijent/istorija.php
+++ Faza5/aplikacija/app/Views/klijent/istorija.php
@@ -13,53 +13,8 @@
 </head>
 
 
-<script>
-    function onTestChange(area) {
-        var key = window.event.keyCode;
-        // If the user has pressed enter
-        if (key === 13) {
-            area.readOnly = "false";
-        }
-    }
 
-    function myFunction(objButton) {
-      
-        var label = objButton.parentNode.parentNode.getElementsByClassName("ocenaLabel")[0];
-        var da = objButton.parentNode.parentNode.getElementsByClassName("ocenaDugme")[0];
-        var ne = objButton.parentNode.parentNode.getElementsByClassName("ocenaDugme")[1];
-        var hid=objButton.parentNode.parentNode.getElementsByClassName("hidden3")[0];
-        
-        label.innerHTML = objButton.innerHTML;
-       
-        da.parentNode.removeChild(da);
-        ne.parentNode.removeChild(ne);
-        
-        if (label.innerHTML == "+") {
-            label.style.color = "green"
-            hid.value=1;
-          
-        } else {
-            label.style.color = "red"
-            hid.value=0;
-      
-        }
-       
-    }
-</script>
 
-<script>
-    function deleteTextArea(button) {
-        var element = button.parentNode.parentNode.getElementsByClassName("komentarinput")[0].disabled = true;
-
-        button.parentNode.removeChild(button);
-    }
-
-    function ukloniPopravku(button) {
-        var zahtev = button.parentNode.parentNode.parentNode.parentNode.parentNode;
-        zahtev.parentNode.removeChild(zahtev);
-        console.log(zahtev);
-    }
-</script>
 
 <body>
 <div class="container-fluid">
--- Faza5/aplikacija/public/js/skriptaIstorija.js
+++ Faza5/aplikacija/public/js/skriptaIstorija.js
@@ -14,3 +14,47 @@ textovi =document.getElementsByClassName('komentarinput');
 }
 
 }
+function myFunction(objButton) {
+      
+    var label = objButton.parentNode.parentNode.getElementsByClassName("ocenaLabel")[0];
+    var da = objButton.parentNode.parentNode.getElementsByClassName("ocenaDugme")[0];
+    var ne = objButton.parentNode.parentNode.getElementsByClassName("ocenaDugme")[1];
+    var hid=objButton.parentNode.parentNode.getElementsByClassName("hidden3")[0];
+    
+    label.innerHTML = objButton.innerHTML;
+   
+    da.parentNode.removeChild(da);
+    ne.parentNode.removeChild(ne);
+    
+    if (label.innerHTML == "+") {
+        label.style.color = "green"
+        hid.value=1;
+      
+    } else {
+        label.style.color = "red"
+        hid.value=0;
+  
+    }
+    var xmlhttp1 = new XMLHttpRequest();
+   
+}
+function deleteTextArea(button) {
+    var element = button.parentNode.parentNode.getElementsByClassName("komentarinput")[0].disabled = true;
+
+    button.parentNode.removeChild(button);
+}
+
+function ukloniPopravku(button) {
+    var zahtev = button.parentNode.parentNode.parentNode.parentNode.parentNode;
+    zahtev.parentNode.removeChild(zahtev);
+    console.log(zahtev);
+}
+
+function onTestChange(area) {
+    var key = window.event.keyCode;
+    // If the user has pressed enter
+    if (key === 13) {
+        area.readOnly = "false";
+    }
+}
+
